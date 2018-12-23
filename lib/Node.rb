bc9d9180f97bf950bcd887f4066485e1da4a7837
Emil Dimitrov <edimitrov1@gmail.com>
2018-12-23 14:04:37 +0200

    Can create linked list

---
+++ lib/Node.rb
@@ -0,0 +1,6 @@
+class Node
+  def initialize(value)
+    @value = value
+    @next = nil
+  end
+end
---
+++ usage.rb
@@ -0,0 +1,8 @@
+require './lib/LinkedList'
+require './lib/Node'
+
+list = LinkedList.new
+
+list.head = Node.new(1)
+
+p list
